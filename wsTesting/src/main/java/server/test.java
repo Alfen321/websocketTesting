c499d22cf9d86c7898a7fc12cdca133b2bd9edfe
david.martin.carl@gmail.com <david.martin.carl@gmail.com>
2018-04-24 10:24:44 +0200

    Working windows console

--- wsTesting/src/main/java/server/test.java
+++ wsTesting/src/main/java/server/test.java
@@ -1,5 +1,10 @@
 package server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -8,32 +13,52 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/test")
 public class test {
-    
+
     Session userSession = null;
-    
-    
+
     @OnOpen
     public void handleOpen(Session userSession) {
         System.out.println("client connected...");
         this.userSession = userSession;
         sendMessage("Welcome");
     }
-    
+
     @OnClose
     public void handleClose() {
         System.out.println("client disconnected...");
     }
-    
+
     @OnMessage
-    public void onMessage(String message) {
-        if(message != null){
+    public void onMessage(String message) throws InterruptedException {
+        String resultMessage = "";
+        if (message != null) {
             System.out.println(message);
-            sendMessage(message);
+            try {
+                Process p = Runtime.getRuntime().exec("cmd /c " + message);
+                InputStream in = p.getInputStream();
+
+                int c;
+                sendMessage("NewCommandReturn");
+                while ((c = in.read()) != -1) {
+                    resultMessage += (char) c;
+                    if(c == 10){
+                        sendMessage(resultMessage);
+                        resultMessage = "";
+                    }
+                }
+                sendMessage("EndCommandReturn");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
-    
+
     public void sendMessage(String message) {
-        this.userSession.getAsyncRemote().sendText(message);
+        try {
+            this.userSession.getBasicRemote().sendText(message);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-    
+
 }
