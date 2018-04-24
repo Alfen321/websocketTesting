7245917e04fcf903443b8d92ddb4dba3224704b5
Alfen321 <tjalfe.m@gmail.com>
2018-04-24 09:35:17 +0200

    send message from server

--- wsTesting/src/main/java/server/test.java
+++ wsTesting/src/main/java/server/test.java
@@ -3,14 +3,20 @@ package server;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/test")
 public class test {
     
+    Session userSession = null;
+    
+    
     @OnOpen
-    public void handleOpen() {
+    public void handleOpen(Session userSession) {
         System.out.println("client connected...");
+        this.userSession = userSession;
+        sendMessage("Welcome");
     }
     
     @OnClose
@@ -19,9 +25,15 @@ public class test {
     }
     
     @OnMessage
-    public String onMessage(String message) {
-        System.out.println(message);
-        return message;
+    public void onMessage(String message) {
+        if(message != null){
+            System.out.println(message);
+            sendMessage(message);
+        }
+    }
+    
+    public void sendMessage(String message) {
+        this.userSession.getAsyncRemote().sendText(message);
     }
     
 }
