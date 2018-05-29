f3d3f4edaee24596f525a493e210ce2725fbfa25
Unknown <tjalfe.m@gmail.com>
2018-05-29 10:33:37 +0200

    fixed observable

--- wsTesting/src/main/java/ConsoleEvironment/Console/ConsoleObservable.java
+++ wsTesting/src/main/java/ConsoleEvironment/Console/ConsoleObservable.java
@@ -6,6 +6,7 @@ import java.util.Observable;
 public class ConsoleObservable extends Observable {
     
     public void sendOutput(String output) {
+        this.setChanged();
         this.notifyObservers(output);
     }
 }
