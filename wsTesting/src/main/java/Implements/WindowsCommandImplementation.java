3f8d914da50bf4a9dea75771fb1ecdce06665dd5
david.martin.carl@gmail.com <david.martin.carl@gmail.com>
2018-06-04 14:41:45 +0200

    Fixes

--- wsTesting/src/main/java/Implements/WindowsCommandImplementation.java
+++ wsTesting/src/main/java/Implements/WindowsCommandImplementation.java
@@ -2,25 +2,24 @@ package Implements;
 
 import Interfaces.DataConnectorInterface;
 import Interfaces.InputCommandInterface;
-
 import java.util.List;
 
 public class WindowsCommandImplementation implements InputCommandInterface {
 
     DataConnectorInterface DataConn = null;
-    List<String> illegalInputs = null;
+    List<String> commandList = null;
 
     public WindowsCommandImplementation() {
         DataConn = new FileImplementation("Windows", "command");
 
-        illegalInputs = DataConn.retrieveIllegalInputs();
+        commandList = DataConn.retrieveIllegalInputs();
     }
 
     public String command(String message){
-        for (String illegalInput : illegalInputs) {
-            System.out.println(illegalInput);
-            if(illegalInput.toLowerCase().contains(message.toLowerCase())){
-                return illegalInput.split("=.=")[1];
+        for (String command : commandList) {
+            System.out.println(command);
+            if(command.toLowerCase().contains(message.toLowerCase())){
+                return command.split("=.=")[1];
             }
         }
         return "No command like this!";
